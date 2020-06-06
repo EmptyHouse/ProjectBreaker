53fa21a4e75e7d34622caf72261826cab1540108
Ryan Andersen <ryan.andersen05@gmail.com>
2020-06-06 02:15:38 -0400

    Companion can now hover. Adding player refrence

--- Source/ProjectBreaker/PlayerClasses/PlayerCompanion.h
+++ Source/ProjectBreaker/PlayerClasses/PlayerCompanion.h
@@ -6,7 +6,7 @@
 #include "GameFramework/Pawn.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/StaticMeshComponent.h"
-
+#include "PlayerCharacter.h"
 
 #include "PlayerCompanion.generated.h"
 
@@ -28,11 +28,22 @@ private:
 	/* The current time for our hover property */
 	float CurrentHoverTimer;
 
+	/* Reference to the Player Character Pawn */
+	APlayerCharacter* TargetPlayerCharacter;
+
+	/* Gets the aim offset vector for our player based on the controller input or mouse position from the companion */
+	FVector GetAimOffset();
+
+	
+
 protected:
 	virtual void BeginPlay() override;
 
-	virtual void UpdateCompanionHover();
+	/* Updates the mesh component offset based on the hover values. (Primarily for visual purposes)*/
+	virtual void UpdateCompanionHover(float DeltaTime);
+	/*Updates the companions position based on the position of the player */
 	virtual void UpdateCompanionPosition();
+	/* Updates the rotation based on the player's aim offset */
 	virtual void UpdateCompanionRotation();
 
 public:	
@@ -58,13 +69,16 @@ public:
 	float HeightOfHover = 20.f;
 
 	/* The rate at which our companion will hover */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "VisualMovment")
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "VisualMovement")
 	float RateOfHover = 25.f;
 
+	/* Capsule Collider Component. Will be RootComponent */
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Component")
 	UCapsuleComponent* CapsuleCollider;
 
 	/* The mesh component of our companion */
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Component")
 	UStaticMeshComponent* AssociatedMesh;
+
+	
 };
--- Source/ProjectBreaker/ProjectBreaker.Build.cs
+++ Source/ProjectBreaker/ProjectBreaker.Build.cs
@@ -10,7 +10,7 @@ public class ProjectBreaker : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Paper2D" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "Paper2D" });
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });

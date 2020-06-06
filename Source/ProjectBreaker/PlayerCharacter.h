123e56e30ce96ce69cbb5f18af3735415a3a328e
aaronthurston27 <41075873+aaronthurston27@users.noreply.github.com>
2020-06-05 19:23:15 -0700

    Added character file and blueprint

---
+++ Source/ProjectBreaker/PlayerCharacter.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "PaperFlipbookComponent.h"
+#include "PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBREAKER_API APlayerCharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};

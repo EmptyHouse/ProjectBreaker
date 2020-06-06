cf96ba0875d2b0a35aaee3579bf7271d172eba0d
Ryan Andersen <ryan.andersen05@gmail.com>
2020-06-05 23:00:30 -0400

    Updates to our companion character

--- Source/ProjectBreaker/PlayerClasses/PlayerCompanion.cpp
+++ Source/ProjectBreaker/PlayerClasses/PlayerCompanion.cpp
@@ -9,6 +9,9 @@ APlayerCompanion::APlayerCompanion()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+
+	RotationRate = 25.f;
+
 }
 
 // Called when the game starts or when spawned
@@ -22,13 +25,11 @@ void APlayerCompanion::BeginPlay()
 void APlayerCompanion::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
 void APlayerCompanion::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 

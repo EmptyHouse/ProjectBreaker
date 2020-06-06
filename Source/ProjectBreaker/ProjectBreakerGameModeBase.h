4431886f2ea2f7e9cc5611cb62a30f068db93f1e
Ryan Andersen <ryan.andersen05@gmail.com>
2020-06-05 22:06:40 -0400

    First Actual commit

---
+++ Source/ProjectBreaker/ProjectBreakerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectBreakerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBREAKER_API AProjectBreakerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
